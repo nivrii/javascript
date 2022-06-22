4987a99df4fdfacdd7c4055cc4c1abff8d9bf709
nivrii <muchokiirvin@gmail.com>
2022-06-22 15:16:27 +0300

    second commit

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,6 @@ const btnClick = document.getElementById("button");
 btnClick.addEventListener("click", printName);
 
 function printName() {
-    var text = document.getElementById('input').value;
-    document.querySelector('p').innerHTML = "Hello ${text}, welcome to the bootcamp"
+    let text = document.getElementById('input').value;
+    document.querySelector('p').innerHTML = 'Hello ${text}, welcome to the bootcamp';
 }
